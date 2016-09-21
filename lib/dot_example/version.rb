30a6d05f1ae44c47550e745541f19c2e0f54f316
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2016-09-21 17:04:09 -0400

    Use pre 1.0 as the version

--- lib/dot_example/version.rb
+++ lib/dot_example/version.rb
@@ -1,5 +1,5 @@
 module DotExample
-  VERSION = "0.9.0"
+  VERSION = "1.0.0.pre"
   SUMMARY = %q(Keep your project's environment variables in snyc across your team.)
-  DATE = %q{2016-9-19}
+  DATE = %q{2016-9-21}
 end
