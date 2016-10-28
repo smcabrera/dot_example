4a18519afde3176163a03efdccc16e2221816520
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2016-10-28 17:52:05 -0400

    No need to support this use case right now

--- spec/lib/dot_example/dot_env_dot_example_spec.rb
+++ spec/lib/dot_example/dot_env_dot_example_spec.rb
@@ -4,25 +4,6 @@ describe DotEnvDotExample do
     before { `touch .env.example` } 
     after { `rm .env.example` }
 
-  describe '.new' do
-    context 'it is initialized without a filename or a dot_env' do
-      it 'uses the defaults of ".env.example" and instantiating a DotEnv with its defaults' do
-        dot_env_dot_example = DotEnvDotExample.new
-        expect(dot_env_dot_example.filename).to eq ".env.example"
-        expect(dot_env_dot_example.dot_env.filename).to eq ".env"
-      end
-    end
-
-    context 'it is initialized with a filename and DotEnv' do
-      it 'uses the given filename' do
-        dot_env = DotEnv.new("spec/support/.env")
-        dot_env_dot_example = DotEnvDotExample.new(filename: ".env.template", dot_env: dot_env)
-        expect(dot_env_dot_example.filename).to eq ".env.template"
-        expect(dot_env_dot_example.dot_env.filename).to eq "spec/support/.env"
-      end
-    end
-  end
-
   describe 'write!' do
     it 'writes the key_lines from the .env file to filename' do
       dot_env = DotEnv.new("spec/support/.env")
